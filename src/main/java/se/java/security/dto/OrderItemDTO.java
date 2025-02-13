78734dee3042c18b72bd09f770180a92b868b3d4
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-02-13 16:43:42 +0100

    updated orderitemdto

--- src/main/java/se/java/security/dto/OrderItemDTO.java
+++ src/main/java/se/java/security/dto/OrderItemDTO.java
@@ -3,11 +3,19 @@ package se.java.security.dto;
 public class OrderItemDTO {
     // vilken produkt
     private String productId;
-
+    private String name;
+    private double price;
     // hur många an produkten har man köpt
     private int quantity;
 
     public OrderItemDTO() {
+    }
+
+    public OrderItemDTO(String productId, String name, double price, int quantity) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
     }
 
     public String getProductId() {
@@ -24,5 +32,21 @@ public class OrderItemDTO {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
