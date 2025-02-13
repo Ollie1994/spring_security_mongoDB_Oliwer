d8642f00a920430488770cc69683ea6f5116c9f4
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-02-13 16:41:49 +0100

    updated ordercontrolelR

--- src/main/java/se/java/security/controllers/OrderController.java
+++ src/main/java/se/java/security/controllers/OrderController.java
@@ -2,10 +2,11 @@ package se.java.security.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import se.java.security.dto.OrderDTO;
 import se.java.security.dto.OrderResponse;
-import se.java.security.models.Order;
+import se.java.security.dto.OrderResponseDTO;
 import se.java.security.services.OrderService;
 
 import java.util.List;
@@ -19,26 +20,28 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    @PostMapping
+    /*@PostMapping
     public ResponseEntity<Order> createOrder(@RequestBody OrderDTO orderDTO) {
         Order newOrder = orderService.createOrder(orderDTO);
         return new ResponseEntity<>(newOrder, HttpStatus.CREATED);
-    }
-
-    /*@GetMapping("/all")
-    public ResponseEntity<List<OrderResponse>> getAllOrders() {
-        List<OrderResponse> orders = orderService.getAllOrders();
-        return ResponseEntity.ok(orders);
     }*/
 
+    @PostMapping
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<OrderResponseDTO> createOrder(@RequestBody OrderDTO orderDTO) {
+        OrderResponseDTO newOrder = orderService.createOrder(orderDTO);
+        return new ResponseEntity<>(newOrder, HttpStatus.CREATED);
+    }
+
     @GetMapping("/all")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<OrderResponse>> getAllOrders() {
         List<OrderResponse> orders = orderService.getAllOrders();
         return ResponseEntity.ok(orders);
     }
 
-
     @GetMapping("/user/{userId}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public ResponseEntity<List<OrderResponse>> getUserOrders(@PathVariable String userId) {
         List<OrderResponse> orders = orderService.getUserOrders(userId);
         return ResponseEntity.ok(orders);
